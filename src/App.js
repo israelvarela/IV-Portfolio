eee6a5d88472518153384f00a9ed5661addc5b42
Izzy V <israelreyvarela@gmail.com>
2022-02-03 01:42:04 -0600

    updating/troubleshooting

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import Header from "./components/Header/Header.js";
+// import Home from "/";
 import About from "./Pages/About";
 import Contact from "./Pages/Contact.js";
 import Projects from "./Pages/Projects.js";
@@ -8,16 +9,18 @@ import Footer from "./components/Footer/footer";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
+// basename={process.env.PUBLIC_URL}
 
 function App() {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router >
       <div className="App">
         <Header />
         <Routes>
-          <Route exact path="/, /about" element={<About/>} />
-          <Route exact path="/projects" element={<Projects/>} />
-          <Route exact path="/contact" element={<Contact/>} />
+          {/* <Route exact path="/" component={Home} /> */}
+          <Route exact path="/,  /about" element={<About/>} />
+          <Route path="/projects" element={<Projects/>} />
+          <Route path="/contact" element={<Contact/>} />
         </Routes>
         <Footer />
       </div>
