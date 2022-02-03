89f660df361ff51c9fc361cb2f32a250a52a9688
Izzy V <israelreyvarela@gmail.com>
2022-02-02 20:15:12 -0600

    updated to react-router-dom V6

---
+++ build/precache-manifest.5cede93e109a5bb91f4e995c7a455061.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ec39553af677fb740044a5445642bc5a",
+    "url": "/iv-portfolio/index.html"
+  },
+  {
+    "revision": "b213fb66cda398fa20b0",
+    "url": "/iv-portfolio/static/css/main.4f46d8ca.chunk.css"
+  },
+  {
+    "revision": "1a81af778df77cfbde68",
+    "url": "/iv-portfolio/static/js/2.567b5075.chunk.js"
+  },
+  {
+    "revision": "52f9d059f32a0a6383e7",
+    "url": "/iv-portfolio/static/js/3.ba42e49a.chunk.js"
+  },
+  {
+    "revision": "b213fb66cda398fa20b0",
+    "url": "/iv-portfolio/static/js/main.8f6ffd46.chunk.js"
+  },
+  {
+    "revision": "c7fa771c370b2ec31fdf",
+    "url": "/iv-portfolio/static/js/runtime~main.cd001f4a.js"
+  },
+  {
+    "revision": "2fee458c69f53421d893312b9b12fbd2",
+    "url": "/iv-portfolio/static/media/IsraelVarela-Resume-2022.2fee458c.pdf"
+  },
+  {
+    "revision": "cfa617eaedc5b2365da08b9acb8e9d0c",
+    "url": "/iv-portfolio/static/media/israelVarelaCommercialHS.cfa617ea.jpg"
+  },
+  {
+    "revision": "b025c589d2a652d6b5e045098659b70e",
+    "url": "/iv-portfolio/static/media/israelVarelaHS2.b025c589.jpg"
+  },
+  {
+    "revision": "0e03bad4ecae6539f6d4136f355a2a12",
+    "url": "/iv-portfolio/static/media/israelVarelaHS3.0e03bad4.jpg"
+  },
+  {
+    "revision": "3096c0742fcf53052374e3a3252fdce6",
+    "url": "/iv-portfolio/static/media/israelVarelaHS4.3096c074.jpg"
+  }
+]);
--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import About from "./Pages/About";
 import Contact from "./Pages/Contact.js";
 import Projects from "./Pages/Projects.js";
 import Footer from "./components/Footer/footer";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 
 function App() {
@@ -13,11 +13,11 @@ function App() {
     <Router basename={process.env.PUBLIC_URL}>
       <div className="App">
         <Header />
-        <Switch>
-          <Route exact path={["/", "/about"]} component={About} />
-          <Route exact path="/projects" component={Projects} />
-          <Route exact path="/contact" component={Contact} />
-        </Switch>
+        <Routes>
+          <Route exact path="/, /about" element={About} />
+          <Route exact path="/projects" element={Projects} />
+          <Route exact path="/contact" element={Contact} />
+        </Routes>
         <Footer />
       </div>
     </Router>
